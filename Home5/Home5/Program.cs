3b9575110e7e3c94383303d416bacff937ea1c83
Mpkhort <giselle98@mail.ru>
2021-06-08 20:31:38 +0300

    add code

---
+++ Home5/Home5/Program.cs
@@ -0,0 +1,117 @@
+﻿using Homework3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home5
+{
+    class Program
+    {
+		static UdpClient server;
+		const int port = 8080;
+		static bool flag;
+        static Socket listeningSocket;
+        static List<IPEndPoint> clients = new List<IPEndPoint>();
+		static Context context = new Context();
+		public static Queue<UdpReceiveResult> queue = new Queue<UdpReceiveResult>();
+
+		static async Task Listener_run()
+		{
+			string reply;
+			while (flag)
+			{
+
+				UdpReceiveResult commandDatagram = await server.ReceiveAsync();
+				queue.Enqueue(commandDatagram);
+				string command = Encoding.UTF8.GetString(commandDatagram.Buffer);
+
+				bool flag_queue = queue.TryDequeue(out commandDatagram);
+				if (flag_queue)
+				{
+					reply = await Request_run(command);
+					Console.WriteLine(reply);
+					byte[] replyDatagram = Encoding.UTF8.GetBytes(reply);
+					await server.SendAsync(replyDatagram, replyDatagram.Length, commandDatagram.RemoteEndPoint);
+				}
+			}
+		}
+		static async Task<string> Request_run( string result){
+
+			string answer = "";	
+			
+						if (result == "1")
+						{
+						Console.WriteLine("get res 1 ");
+						Console.WriteLine(result);
+							IQueryable<Passanger> passanger = from ps in context.Passanger select ps;
+							List<Passanger> list_passanger = passanger.ToList();
+							foreach (Passanger passangers in list_passanger) {
+								Console.WriteLine($"Name - {passangers.Name} LastName - {passangers.LastName} Email - {passangers.Email}");
+						        answer += $"Name - {passangers.Name} LastName - {passangers.LastName} Email - {passangers.Email}\n";
+					        }
+
+				        }
+						else if (result == "2")
+						{
+						    Console.WriteLine("get res 2 ");
+						    IQueryable<Flight> flight = from ps in context.Flight select ps;
+							List<Flight> list_flight = flight.ToList();
+					        foreach (Flight flights in list_flight) {
+						          Console.WriteLine($"Name - {flights.Name} Price - {flights.Cost}");
+						          answer += $"Name - {flights.Name} Price - {flights.Cost}\n";
+							}
+						}
+			//context.Dispose();
+			return answer;
+		}
+
+		static async Task Listener()
+		{
+			await Task.Run(() => Listener_run());
+		}
+
+		static void add_notes() {
+			context.Passanger.Add(new Passanger()
+			{
+				Name = "Yorick",
+				LastName = "Ivanov",
+				Email = "kyky@mail.ru"
+			});
+			context.Passanger.Add(new Passanger()
+			{
+				Name = "Ilya",
+				LastName = "Semenov",
+				Email = "semy@mail.ru"
+			});
+			context.Flight.Add(new Flight()
+			{
+				Name = "YTY2",
+				Cost = 27000
+			});
+			context.Flight.Add(new Flight()
+			{
+				Name = "OGO3",
+				Cost = 5600
+			});
+			context.SaveChanges();
+		}
+
+		static async Task Main(string[] args)
+        {
+		    server = new UdpClient(port);
+			Console.WriteLine("Server start");
+		    context.Database.EnsureCreated();
+			add_notes();
+			context.SaveChanges();
+			listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			flag = true;
+			Task listener = Listener();
+			Console.ReadLine();
+			await listener;
+		}
+    }
+}
---
+++ Homework5_client/Homework4_client/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static int remotePort;
+        static IPAddress ipAddress;
+        static Socket listeningSocket;
+        static UdpClient client;
+        static bool flag = true;
+        static IPEndPoint serverEP;
+        static void Main(string[] args)
+        {
+            ipAddress = IPAddress.Loopback;
+            remotePort = 8080;
+            client = new UdpClient();
+            serverEP = new IPEndPoint(ipAddress, remotePort);
+            Console.WriteLine("Enter the num: 1 - First request, 2 - Second request, 3 - Exit");
+            listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                while (flag)
+                {
+                    string cmd = Console.ReadLine();
+                    switch (cmd)
+                    {
+                        case "1":
+                             Console.WriteLine("You choisen 1 request");
+                             Request1(cmd);
+                        break;
+                        case "2":
+                             Console.WriteLine("You choisen 2 request");
+                             Request1(cmd);
+                        break;
+                        case "3":
+                            Console.WriteLine("Closing programm...");
+                            flag = false;
+                            Close();
+                            break;
+                        default:
+                            Console.WriteLine("Incorrect value, try again");
+                            break;
+                    }
+                }
+        }
+        public static async void Request1(string cmd)
+        {
+            Console.WriteLine("req in progress...");
+            string ans = await SendAndReceiveCommand(cmd);
+            Console.WriteLine(ans);
+        }
+
+        static async Task<string> SendAndReceiveCommand(string command)
+        {
+            byte[] commandDatagram = Encoding.UTF8.GetBytes(command);
+            await client.SendAsync(commandDatagram, commandDatagram.Length, serverEP);
+            UdpReceiveResult result = await client.ReceiveAsync();
+            string reply = Encoding.UTF8.GetString(result.Buffer);
+            return reply;
+        }
+
+        private static void Close()
+        {
+            if (listeningSocket != null)
+            {
+                listeningSocket.Shutdown(SocketShutdown.Both);
+                listeningSocket.Close();
+                listeningSocket = null;
+            }
+
+            Console.WriteLine("Server closed!");
+        }
+    }
+}
